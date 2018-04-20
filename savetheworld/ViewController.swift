2eab9a13904f558bd3b5e0e6ec7f6cff29e173e7
Gurpreetsinghk <gkandola16@gmail.com>
2018-04-20 14:51:09 -0400

    Initial Commit

---
+++ savetheworld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  savetheworld
+//
+//  Created by gurpreet singh on 2018-04-20.
+//  Copyright © 2018 Gurpreetsingh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
